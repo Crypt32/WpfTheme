d25c6bcfecc9ddefa70f14ff74c3022e39d25e0a
Vadims Podāns <vpodans@sysadmins.lv>
2020-12-15 21:49:42 +0200

    added support for default button

--- WpfTheme/Toolkit/MsgBox.cs
+++ WpfTheme/Toolkit/MsgBox.cs
@@ -3,15 +3,20 @@ using System.Windows;
 
 namespace SysadminsLV.WPF.OfficeTheme.Toolkit {
     public class MsgBox {
-        public static MessageBoxResult Show(String header, String message, MessageBoxImage image = MessageBoxImage.Error, MessageBoxButton button = MessageBoxButton.OK) {
+        public static MessageBoxResult Show(
+            String header,
+            String message,
+            MessageBoxImage image = MessageBoxImage.Error,
+            MessageBoxButton button = MessageBoxButton.OK,
+            MessageBoxResult defaultResult = MessageBoxResult.None) {
             WindowCollection windows = Application.Current.Windows;
             Window hwnd = null;
             if (windows.Count > 0) {
                 hwnd = windows[windows.Count - 1];
             }
             return hwnd == null
-                ? MessageBox.Show(message, header, button, image)
-                : MessageBox.Show(hwnd, message, header, button, image);
+                ? MessageBox.Show(message, header, button, image, defaultResult)
+                : MessageBox.Show(hwnd, message, header, button, image, defaultResult);
         }
     }
 }
