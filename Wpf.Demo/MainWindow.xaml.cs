dceef9f5e59f0051aac752be7b23d439b068b807
Vadims Podāns <vpodans@sysadmins.lv>
2022-10-02 13:46:31 +0300

    fixed broken ListView.ItemContainerStyle and ListBox.ItemContainerStyle

--- Wpf.Demo/MainWindow.xaml.cs
+++ Wpf.Demo/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.Cryptography;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Wpf.Demo {
     /// <summary>
@@ -20,6 +9,8 @@ namespace Wpf.Demo {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            ListView.Items.Add(new Oid("sha1"));
+            ListView.Items.Add(new Oid("sha256"));
         }
     }
 }
