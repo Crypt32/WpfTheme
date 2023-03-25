fc26edb0699d3e5e7c502ebcc4fb20a46fd3f5b1
Vadims Podāns <vpodans@sysadmins.lv>
2023-03-25 22:06:48 +0200

    drafted Expander style

---
+++ Wpf.Demo/Views/UserControls/ExpanderUC.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Wpf.Demo.Views.UserControls {
+    /// <summary>
+    /// Interaction logic for ExpanderUC.xaml
+    /// </summary>
+    public partial class ExpanderUC {
+        public ExpanderUC() {
+            InitializeComponent();
+        }
+    }
+}
