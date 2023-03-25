7e952404d0ee9503978a4ed56ba288d287c3861d
Vadims Podāns <vpodans@sysadmins.lv>
2023-03-25 12:41:20 +0200

    updated textbox validation error style

---
+++ Wpf.Demo/ViewModels/FormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SysadminsLV.WPF.OfficeTheme.Toolkit.ViewModels;
+
+namespace Wpf.Demo.ViewModels;
+class FormViewModel : ViewModelBase {
+    Int32 number;
+
+    public Int32 Number {
+        get => number;
+        set {
+            number = value;
+            OnPropertyChanged(nameof(Number));
+        }
+    }
+}
