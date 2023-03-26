10d0b1a8aa3f5be078f64aa4a4a756d325714af2
Vadims Podāns <vpodans@sysadmins.lv>
2023-03-26 17:50:29 +0300

    merged ToolBar different button styles to single style

---
+++ Wpf.Demo/Views/UserControls/ButtonUC.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Wpf.Demo.Views.UserControls;
+/// <summary>
+/// Interaction logic for ButtonUC.xaml
+/// </summary>
+public partial class ButtonUC {
+    public ButtonUC() {
+        InitializeComponent();
+    }
+}
