cb975726da4f1842c8d0b1a852016a4cb07578d5
Vadims Podāns <vpodans@sysadmins.lv>
2023-03-29 14:09:51 +0300

    added property IsShown to easily control LoadingSpinner visibility

--- WpfTheme/Controls/LoadingSpinner.cs
+++ WpfTheme/Controls/LoadingSpinner.cs
@@ -1,5 +1,20 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace SysadminsLV.WPF.OfficeTheme.Controls {
-    public class LoadingSpinner : Control { }
+    public class LoadingSpinner : Control {
+        public static readonly DependencyProperty IsShownProperty = DependencyProperty.Register(
+            nameof(IsShown),
+            typeof(Boolean),
+            typeof(LoadingSpinner),
+            new FrameworkPropertyMetadata(false));
+        /// <summary>
+        /// indicates whether the control should show or not.
+        /// </summary>
+        public Boolean IsShown {
+            get => (Boolean)GetValue(IsShownProperty);
+            set => SetValue(IsShownProperty, value);
+        }
+    }
 }
