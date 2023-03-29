e985c3fa0fe255436bc2d91b8dcabc26a9da9600
Vadims Podāns <vpodans@sysadmins.lv>
2023-03-29 11:24:09 +0300

    added progressbars to demo

---
+++ Wpf.Demo/Views/UserControls/ProgressBarUC.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Wpf.Demo.Views.UserControls;
+/// <summary>
+/// Interaction logic for ProgressBarUC.xaml
+/// </summary>
+public partial class ProgressBarUC {
+    public ProgressBarUC() {
+        InitializeComponent();
+    }
+}
