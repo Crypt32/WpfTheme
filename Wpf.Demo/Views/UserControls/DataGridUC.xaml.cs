d4c6b50ad873a94187bca0c287284aa41c6ee5cd
Vadims Podāns <vpodans@sysadmins.lv>
2023-05-06 13:56:51 +0300

    added DataGrid demo

---
+++ Wpf.Demo/Views/UserControls/DataGridUC.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Wpf.Demo.Views.UserControls;
+/// <summary>
+/// Interaction logic for DataGridUC.xaml
+/// </summary>
+public partial class DataGridUC {
+    public DataGridUC() {
+        InitializeComponent();
+        var list = new List<Oid> {
+                                     new Oid("sha1"),
+                                     new Oid("sha256"),
+                                     new Oid("sha384"),
+                                     new Oid("sha512")
+                                 };
+        Grid1.ItemsSource = list;
+        Grid2.ItemsSource = list;
+    }
+}
