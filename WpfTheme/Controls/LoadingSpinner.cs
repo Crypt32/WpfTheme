3fd096f471e1f1c819cae91bdd556e0018901159
Vadims Podāns <vpodans@sysadmins.lv>
2023-06-08 16:34:58 +0300

    fixed issue with spinner

--- WpfTheme/Controls/LoadingSpinner.cs
+++ WpfTheme/Controls/LoadingSpinner.cs
@@ -4,6 +4,19 @@ using System.Windows.Controls;
 
 namespace SysadminsLV.WPF.OfficeTheme.Controls {
     public class LoadingSpinner : Control {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text),
+            typeof(String),
+            typeof(LoadingSpinner),
+            new FrameworkPropertyMetadata(null));
+        /// <summary>
+        /// indicates whether the control should show or not.
+        /// </summary>
+        public String Text {
+            get => (String)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
         public static readonly DependencyProperty IsShownProperty = DependencyProperty.Register(
             nameof(IsShown),
             typeof(Boolean),
