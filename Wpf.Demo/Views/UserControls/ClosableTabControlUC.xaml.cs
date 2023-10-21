4fdf6b72e62934bb0da1304b44e1efb2611c2fa6
Vadims Podāns <vpodans@sysadmins.lv>
2023-10-21 20:54:49 +0300

    added closable tab demo

---
+++ Wpf.Demo/Views/UserControls/ClosableTabControlUC.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Wpf.Demo.Views.UserControls;
+/// <summary>
+/// Interaction logic for ClosableTabControl.xaml
+/// </summary>
+public partial class ClosableTabControlUC {
+    public ClosableTabControlUC() {
+        InitializeComponent();
+        Tabs.Add(new TabVM { Header = "Hello", Content = "Hello2" });
+    }
+
+    public ObservableCollection<TabVM> Tabs { get; } = new();
+}
+public class TabVM {
+    public String Header { get; set; }
+    public String Content { get; set; }
+}
