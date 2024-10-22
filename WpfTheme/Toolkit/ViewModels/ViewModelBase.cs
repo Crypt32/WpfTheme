8b643993ecc170cd50c5ad4a8be9a30735dca3a1
Vadims Podāns <vpodans@sysadmins.lv>
2024-10-22 11:41:00 +0300

    improved ViewModelBase

--- WpfTheme/Toolkit/ViewModels/ViewModelBase.cs
+++ WpfTheme/Toolkit/ViewModels/ViewModelBase.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-namespace SysadminsLV.WPF.OfficeTheme.Toolkit.ViewModels {
-    public abstract class ViewModelBase : INotifyPropertyChanged {
-        protected virtual void OnPropertyChanged(String PropertyName = null) {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            handler?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
+namespace SysadminsLV.WPF.OfficeTheme.Toolkit.ViewModels;
+/// <summary>
+/// Represents the base view model that implements <see cref="INotifyPropertyChanged"/> interface.
+/// </summary>
+public abstract class ViewModelBase : INotifyPropertyChanged {
+    /// <summary>
+    /// Raises property changed event.
+    /// </summary>
+    /// <param name="propertyName">Property name that changed its value.</param>
+    protected virtual void OnPropertyChanged([CallerMemberName] String? propertyName = null) {
+        PropertyChangedEventHandler handler = PropertyChanged;
+        handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+    /// <inheritdoc />
+    public event PropertyChangedEventHandler? PropertyChanged;
 }
