50eb557a88c505008db6b79b9d9e848419172411
Vadims Podāns <vpodans@sysadmins.lv>
2025-11-19 15:00:38 +0200

    adjusted TreeViewItem selection active and inactive colors to make them softer

---
+++ Wpf.Demo/Views/UserControls/TreeViewUC.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Wpf.Demo.Views.UserControls;
+/// <summary>
+/// Interaction logic for TreeViewUC.xaml
+/// </summary>
+public partial class TreeViewUC {
+    public TreeViewUC() {
+        InitializeComponent();
+    }
+}
